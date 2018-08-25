5f193eef369f4f03d8ce09c05c9dfcbf5d7c0feb
shabbyjoon <samokhtarani@yahoo.com>
2018-08-24 17:34:15 -0700

    Done with intro and creating database and tables. Fixed test cases

--- server/schema.sql
+++ server/schema.sql
@@ -4,10 +4,27 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(20),
+  messageText VARCHAR (140),
+  chatroom VARCHAR (20),
+  timePosted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  primary key (id)
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE usernames (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(20),
+  primary key (id)
+);
+
+CREATE TABLE chatrooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  chatroom VARCHAR (20),
+  primary key (id)
+);
 
 
 
